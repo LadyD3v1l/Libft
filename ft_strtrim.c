5a9e723e34f279ff8a0e1cbed1d58f9173de5c56
LadyD3v1l <melissamarquespt@hotmail.com>
2025-04-15 13:41:56 +0100

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,18 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jobraga <jobraga@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jobraga- <jobraga-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 19:13:07 by jobraga           #+#    #+#             */
-/*   Updated: 2025/04/14 19:13:07 by jobraga          ###   ########.fr       */
+/*   Updated: 2025/04/15 13:40:51 by jobraga-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_check(char const *set, char const c)
+{
+	size_t		i;
+
+	i = 0;
+	while (set[i] != '\0')
+	{
+		if (c == set[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	cont;
 	size_t	len_i;
 	size_t	len_f;
 	char	*rest;
@@ -22,27 +35,20 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (!s1 || !set)
 		return (NULL);
 	len_i = 0;
-	len_f = ft_strlen(s1) - 1;
-	while (s1[len_i] == set[0])
+	while (s1[len_i] && ft_check(set, s1[len_i]))
 		len_i++;
-	while (s1[len_f] == set[0])
+	len_f = ft_strlen(s1) - 1;
+	while (len_f != 0 && ft_check(set, s1[len_f]))
 		len_f--;
-	rest = malloc(sizeof(char) * (len_f - len_i + 1));
-	if (!rest)
-		return (NULL);
-	cont = 0;
-	while ((len_i + cont) < len_f)
-	{
-		rest[cont] = s1[len_i + cont];
-		cont++;
-	}
-	rest[cont] = '\0';
+	rest = ft_substr(s1, len_i, (len_f - len_i));
 	return (rest);
 }
-int		main(void)
-{
-	char str[] = "......Tenta d.e novo ai    ........";
-	char chat[] = ".";
 
-	printf("Essa é a frase completa '%s', tirando os '%s'. Fica '%s'.\n", str, chat, ft_strtrim(str, chat));
-}
+// int		main(void)
+// {
+// 	char str[] = "abcbcacba   Tenta d.e novo ai    abcbcacba";
+// 	char chat[] = "abc";
+
+// 	printf("Essa é a frase completa '%s', tirando os '%s'. 
+// Fica '%s'.\n", str, chat, ft_strtrim(str, chat));
+// }
