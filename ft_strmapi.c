f35bd55a4d98704275a50b4b4eb28d51c0e34f81
LadyD3v1l <melissamarquespt@hotmail.com>
2025-04-18 16:04:24 +0100

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -19,7 +19,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	size_t	i;
 
 	i = 0;
-	if(!s || !f)
+	if (!s || !f)
 		return (NULL);
 	len = ft_strlen(s);
 	str = malloc(len + 1);
